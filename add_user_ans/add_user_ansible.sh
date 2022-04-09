0f271e6eb35c4d9e9138f6baad567d1b3bb612c3
mhanief <hanief.harun@gmail.com>
2022-04-10 05:31:27 +0800

    commit

--- add_user_ans/add_user_ansible.sh
+++ add_user_ans/add_user_ansible.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 useradd -s /bin/bash -d /home/ans -m ans
 mkdir /home/ans/.ssh
 chmod 700 /home/ans/.ssh
 touch /home/ans/.ssh/authorized_keys
-
+chmod 600 ~/.ssh/authorized_keys
+curl https://raw.githubusercontent.com/mhanief/my-ansible/main/add_user_ans/ans_pub > /home/ans/.ssh/authorized_keys
+chown ans. /home/ans/ -R
