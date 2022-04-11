a64b14b6eca54ca4caf1e2e915510b7098f3a086
mhanief <hanief.harun@gmail.com>
2022-04-12 00:36:07 +0800

    update config

--- machine_setup/setup_ip.sh
+++ machine_setup/setup_ip.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-hostname=localhost.localdomain
-ipadd=XXX.XXX.XXX.XXX/XX
-gw=XXX.XXX.XXX.XXX
-dns=XXX.XXX.XXX.XXX
-nmcli general hostname ansible06-db03.sh.local
-nmcli con mod enp0s3 ipv4.address 192.168.88.106/24
-nmcli con mod enp0s3 ipv4.gateway 192.168.88.1
-nmcli con mod enp0s3 ipv4.dns 8.8.8.8
+set_hostname=ansible06-db03.sh.local
+set_ip=XXX.XXX.XXX.XXX/XX
+set_gw=XXX.XXX.XXX.XXX
+set_dns=XXX.XXX.XXX.XXX
+nmcli general hostname $set_hostname
+nmcli con mod enp0s3 ipv4.address $set_ip
+nmcli con mod enp0s3 ipv4.gateway $set_gw
+nmcli con mod enp0s3 ipv4.dns $set_dns
 nmcli con mod enp0s3 ipv4.method manual
 nmcli con mod enp0s3 connection.autoconnect yes
 nmcli con up enp0s3
-echo -e "\n--> Below are your machine configuration: \n\nHostname: $(hostname -f)\n\n$(nmcli -p)"
