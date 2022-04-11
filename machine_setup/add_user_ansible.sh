f48d8f85db3cccb244a53463fb9aeb51950c156b
mhanief <hanief.harun@gmail.com>
2022-04-12 00:06:05 +0800

    update config

--- machine_setup/add_user_ansible.sh
+++ machine_setup/add_user_ansible.sh
@@ -5,7 +5,7 @@ mkdir /home/ans/.ssh
 chmod 700 /home/ans/.ssh
 touch /home/ans/.ssh/authorized_keys
 chmod 600 /home/ans/.ssh/authorized_keys
-curl https://raw.githubusercontent.com/mhanief/my-ansible/main/add_user_ans/ans_pub > /home/ans/.ssh/authorized_keys
+curl https://raw.githubusercontent.com/mhanief/my-ansible/main/machine_setup/ans_pub > /home/ans/.ssh/authorized_keys
 chown ans. /home/ans/ -R
 # Grant sudo without password to user ans
 touch /etc/sudoers.d/ansible
